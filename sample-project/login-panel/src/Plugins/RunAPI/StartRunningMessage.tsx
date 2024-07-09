663724f41b09bd255474b3b98487ce21c46c0e75
Simpson561 <1783689461@qq.com>
2024-07-10 02:01:25 +0800

    Update StartRunningMessage.tsx

--- sample-project/login-panel/src/Plugins/RunAPI/StartRunningMessage.tsx
+++ sample-project/login-panel/src/Plugins/RunAPI/StartRunningMessage.tsx
@@ -4,12 +4,15 @@ export class SubmitRunningMessage extends RunMessage {
     student_id: number;
     startTime: Date;
     finishTime: Date;
-    distance: Double;
-    png: bytea;
+    distance: number;
+    png: Uint8Array;
 
-    constructor(student_id:number) {
+    constructor(student_id: number, startTime: Date, finishTime: Date, distance: number, png: Uint8Array) {
         super();
         this.student_id = student_id;
-
+        this.startTime = startTime;
+        this.finishTime = finishTime;
+        this.distance = distance;
+        this.png = png;
     }
 }
